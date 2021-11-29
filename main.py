bfa1ef28b5f6dd10f7ddb25a8860c595bf79502a
Lapwell <76802132+Lapwell@users.noreply.github.com>
2021-11-28 21:27:44 -0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+import discord
+import requests
+
+# TODO: Query "eve who" to check new users if they're in the corp. If so, give them the member role, if not leave them alone. Check this every hour or when someone joins the server.
+# TODO: Possible music capabilities?
+# TODO: Possible price checks in the trade hubs?
+
+
